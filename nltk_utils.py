3562e065e104fb67afea7f5844d5a391ec4bbd0d
Waseemarshad97 <wakk97@gmail.com>
2023-07-13 05:20:08 +0100

    feat:coded utility functions and training data

---
+++ nltk_utils.py
@@ -0,0 +1,27 @@
+import nltk
+
+# nltk.download("punkt")
+from nltk.stem.porter import PorterStemmer
+
+stemmer = PorterStemmer()
+
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
+
+
+# a = "my fist ai project"
+# print(a)
+# a = tokenize(a)
+# print(a)
+# words = ["organ", "organise", "organizing"]
+# stemmed = [stem(w) for w in words]
+# print(stemmed)
