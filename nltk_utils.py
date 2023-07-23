c6b8b229ff4bfc91450553f5ebad7f05ed0b3934
Waseemarshad97 <wakk97@gmail.com>
2023-07-23 05:51:46 +0100

    model done

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 
 # nltk.download("punkt")
 from nltk.stem.porter import PorterStemmer
@@ -15,7 +16,12 @@ def stem(word):
 
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
 
 
 # a = "my fist ai project"
--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 import json
 from nltk_utils import tokenize, stem, bag_of_words
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset,dataloader
 
 with open("intents.json", "r") as f:
     intents = json.load(f)
@@ -20,7 +24,7 @@ ignore_words = ["?", "!", ".", ","]
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-print(tags)
+# print(tags)
 
 x_train = []
 y_train = []
@@ -31,3 +35,22 @@ for pattern_sentence, tag in xy:
 
     label = tags.index(tag)
     y_train.append(label)
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(x_train)
+        self.x_data = x_train
+        self.y_data = y_train
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]   
+    def __len__(self):
+        return self.n_samples
+    
+    batch_size = 8
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset= dataset, batch_size=batch_size, shuffle: True, num_workers= 0)
